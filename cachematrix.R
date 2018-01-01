4a0f6c527b88496bd22cbf7678f7bc47171eca21
ajdscc <ajdscc@hotmail.com>
2018-01-01 15:27:10 -0200

    Included error messages

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## The first function, makeVector creates a special "vector", 
-## which is really a list containing a function to
-## 1. set the value of the vector
-## 2. get the value of the vector
+## The first function, makeCacheMatrix creates a special "matrix" object that can cache its inverse, 
+## which is really a list containing functions to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
 ## 3. set the value of the inverse
 ## 4. get the value of the inverse
+##
+## and the cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -23,7 +25,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function calculates the inverse of the special "vector" 
+## The following function calculates the inverse of the special "matrix" object 
 ## created with the above function. 
 ## However, it first checks to see if the inverse has already been calculated.
 ## If so, it gets the inverse from the cache and skips the computation.
@@ -31,8 +33,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m <- x$getsolve()
+  ## Return a matrix that is the inverse of makeCacheMatrix object
+  
+  if (!is.list(x)) {
+    message("cacheSolve(x,...): 'x' must be a special 'list_obj' returned by MakeCacheMatrix(x = matrix())")
+    return(invisible(NULL))
+  }
+  
+  if (length(x$get()) ==1) {
+    message("Matrix not set. Store the matrix using 'list_obj'$set(x = matrix()); 'list_obj' is the list returned by MakeCacheMatrix(x = matrix())")
+    return(invisible(NULL))
+  }
+  
+  m <- x$getsolve()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
